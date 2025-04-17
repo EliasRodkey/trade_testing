a6137baf78d5ae4a890add51da0f177f8ba2e959
Elias Rodkey <elias.rodkey@gmail.com>
2025-04-16 19:16:02 -0700

    adds learning materials to the git repository

---
+++ Learning/algorithmic_trading_with_python/listings/chapter_2/2_1_return_series_pure_python.py
@@ -0,0 +1,10 @@
+from typing import List
+def calculate_return_series(prices: List[float]) -> List[float]:
+    """
+    Calculates return series as a parallel list of returns on prices
+    """
+    return_series = [None]
+    for i in range(1, len(prices)):
+        return_series.append((prices[i] / prices[i-1]) - 1)
+
+    return return_series
